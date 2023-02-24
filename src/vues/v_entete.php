5d1ff159f32e3dbb2a335ebce34229507c851f22
Reednar <lepieloic94@gmail.com>
2023-02-24 15:14:35 +0100

    Maj templace mvc

--- src/vues/v_entete.php
+++ src/vues/v_entete.php
@@ -8,13 +8,13 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="public/css/footer.css">
-    <title>Morpion</title>
+    <title>Template MVC</title>
 </head>
 
 <body>
     <nav class="navbar navbar-expand-lg bg-body-tertiary">
         <div class="container-fluid">
-            <a class="navbar-brand" href="index.php?uc=accueil">Morpion</a>
+            <a class="navbar-brand" href="index.php?uc=accueil">MVC</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
